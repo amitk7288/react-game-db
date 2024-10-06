305181c868c44fa05de40e2e1f07db8bdcc962b8
amitk7288 <amitkadara@gmail.com>
2024-10-06 22:44:14 +0100

    updated the header section

--- src/components/mainview/MainView.jsx
+++ src/components/mainview/MainView.jsx
@@ -1,10 +1,10 @@
 export default function MainView() {
 
   return (
-    <main className="w-full bg-green-200 lg:w-[calc(100%_-_25vw)] 2xl:w-[calc(100%_-_20vw)]">
-      <div className="dark:border-drkbrd dark:bg-drkbg dark:text-drkcol no-scrollbar fixed right-0 top-[80px] z-[-1] h-[calc(100%_-_80px)] w-full overflow-y-scroll border-x px-4 pb-4 pt-4 sm:px-5 sm:pt-5 lg:w-[calc(100%_-_25vw)] 2xl:w-[calc(100%_-_20vw)] bg-green-200">
+    <main className="w-full lg:w-[calc(100%_-_20vw)] 2xl:w-[calc(100%_-_20vw)]">
+      <div className="dark:bg-drkbg dark:text-drkcol no-scrollbar fixed right-0 top-[80px] z-[-1] h-[calc(100%_-_80px)] w-full overflow-y-scroll px-4 pb-4 pt-4 sm:px-5 sm:pt-5 lg:w-[calc(100%_-_20vw)] 2xl:w-[calc(100%_-_20vw)]">
         <div className="mb-5 flex items-center gap-3">
-          <p>hello</p>
+          <p>React Game DB</p>
         </div>
       </div>
     </main>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <aside className="fixed bottom-0 left-0 top-0 z-0 hidden h-[100%] w-[25vw] lg:flex 2xl:w-[20vw] bg-red-100">
+    <aside className=" dark:bg-drkbg dark:text-drkcol fixed bottom-0 left-0 top-0 z-0 hidden h-[100%] w-[20vw] bg-red-100 lg:flex 2xl:w-[20vw]">
       <p>sidebar menu</p>
     </aside>
   );
