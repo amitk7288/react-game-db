9ef9cc1af4e645f15225ec24399f6746efcc6657
devdreamercode <devdreamercode@gmail.com>
2025-03-22 12:34:44 +0000

    ui comp tests

--- src/components/ui-components/CardGridPage.jsx
+++ src/components/ui-components/CardGridPage.jsx
@@ -2,7 +2,10 @@ export default function CardGridPage({ children, title, desc, icon }) {
   return (
     <div className="mb-6 flex flex-col gap-3 p-2">
       <div className="mb-6 flex flex-col items-start gap-2">
-        <div className="flex items-center gap-2 text-[60px] font-semibold dark:text-white flex-wrap">
+        <div
+          data-testid="card-grid-page-iconTitle-container"
+          className="flex flex-wrap items-center gap-2 text-[60px] font-semibold dark:text-white"
+        >
           {icon}
           <p className="capitalize leading-tight">{title}</p>
         </div>
